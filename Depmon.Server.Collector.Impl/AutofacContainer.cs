21c8868e9efea4785d6790d7db4217bc33582de3
Abylay Makhmetzhan <ablai96@gmail.com>
2015-09-29 14:24:39 +0600

    Autofac конфиг исправил

--- Depmon.Server.Collector.Impl/AutofacContainer.cs
+++ Depmon.Server.Collector.Impl/AutofacContainer.cs
@@ -20,17 +20,17 @@ namespace Depmon.Server.Collector.Impl
 
             builder.Register(s => new UnitOfWork(getConnectionString("depmon"))).As<IUnitOfWork>().InstancePerLifetimeScope();
             
-            builder.Register(s => new Engine(s.Resolve<IObjectFactory>())).As<IEngine>();
+            builder.RegisterType<Engine>().As<IEngine>();
 
             builder.RegisterType<ConfigReader>().As<IConfigReader>();
             builder.RegisterType<MailReciever>().As<IMailReciever>();
             builder.RegisterType<CsvParse>().As<ICsvParse>();
             
-            builder.Register(s => new FactsSave(s.Resolve<IRepository<Report>>(), s.Resolve<IRepository<Fact>>())).As<IFactsSave>();
+            builder.RegisterType<FactsSave>().As<IFactsSave>();
 
-            builder.Register(s => new FactRepository(s.Resolve<IUnitOfWork>())).As<IRepository<Fact>>();
-            builder.Register(s => new ReportRepository(s.Resolve<IUnitOfWork>())).As<IRepository<Report>>();
-            
+            builder.RegisterType<FactRepository>().As<IRepository<Fact>>();
+            builder.RegisterType<ReportRepository>().As<IRepository<Report>>();
+
             builder.Register(s => new AutofacObjectFactory(_container)).As<IObjectFactory>();
 
             _container = builder.Build();
