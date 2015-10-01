f811abdc56fc6481a8ba78764472630c5d9b240e
Sergey Rubtsov <rsv-astana@mail.ru>
2015-10-01 19:03:17 +0600

    Проверка, не пустой ли отчет

--- Depmon.Server.Collector.Impl/ReportRegistry.cs
+++ Depmon.Server.Collector.Impl/ReportRegistry.cs
@@ -23,6 +23,12 @@ namespace Depmon.Server.Collector.Impl
             {
                 using (var transaction = _unitOfWork.BeginTransaction())
                 {
+                    if (facts.Length == 0 || string.IsNullOrWhiteSpace(facts[0].SourceCode))
+                    {
+                        Console.WriteLine("Warning!: facts or SourceCode is empty");
+                        return;
+                    }
+
                     var report = new Report { Id = 0, CreatedAt = DateTime.Now, SourceCode = facts[0].SourceCode, IsLast = true };
                     var reportId = _reportRepository.Save(report);
 
