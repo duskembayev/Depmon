f48adb21302a4e592c529b345071dfa595787282
Sergey Rubtsov <rsv-astana@mail.ru>
2015-10-01 19:21:54 +0600

    Проверка вынесена за транзакцию

--- Depmon.Server.Collector.Impl/ReportRegistry.cs
+++ Depmon.Server.Collector.Impl/ReportRegistry.cs
@@ -19,16 +19,15 @@ namespace Depmon.Server.Collector.Impl
 
         public void Save(Fact[] facts)
         {
+            if (facts.Length == 0 || string.IsNullOrWhiteSpace(facts[0].SourceCode))
+            {
+                Console.WriteLine("facts or SourceCode is empty");
+                return;
+            }
             try
             {
                 using (var transaction = _unitOfWork.BeginTransaction())
                 {
-                    if (facts.Length == 0 || string.IsNullOrWhiteSpace(facts[0].SourceCode))
-                    {
-                        Console.WriteLine("Warning!: facts or SourceCode is empty");
-                        return;
-                    }
-
                     var report = new Report { Id = 0, CreatedAt = DateTime.Now, SourceCode = facts[0].SourceCode, IsLast = true };
                     var reportId = _reportRepository.Save(report);
 
